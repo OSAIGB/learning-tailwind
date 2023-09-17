8980daf20c2487a97859efdf39398987f52456c4
unknown <osaigbovohumphrey@gmail.com>
2023-09-17 12:08:16 +0100

    third commit

--- src/body.tsx
+++ src/body.tsx
@@ -6,9 +6,9 @@ import hotel2 from './pics/hotel-2.jpg'
 const Body: React.FC =() =>{
 return(
         <div> 
-            <div className="grid lg:grid-cols-2 gap-8 pl-4 pr-4 pt-8 md:pl-20 md:pr-20 md:pt-28 lg:pt-32 bg-gray-50">
+            <div className="grid lg:grid-cols-2 gap-8 pl-4 pr-4 pt-12 md:pl-20 md:pr-20 md:pt-28 lg:pt-32 bg-gray-50">
 
-                <div className="">
+                <div className="sm:pt-12 md:pt-0">
                     <img src={hotel1} alt="img-1" className=" rounded-lg hover:scale-105 transition duration-300 cursor-pointer "/>
                     <p className='pt-3 text-xl font-mono text-blue-500'>Beach Photos</p>
                 <p className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
@@ -20,9 +20,9 @@ return(
                  </div>
             </div>
             <div className="grid md:grid-cols-1 lg:grid-cols-3 
-            sm:grid-cols-1 gap-7  pl-4 pr-4 xl:p-16 md:pl-30 md:pr-30 bg-gray-50 ">
+            sm:grid-cols-1 gap-7  pl-4 pr-4 xl:p-16 md:pl-20 md:pr-20 bg-gray-50 ">
   {gridData.map((items, index) => (
-    <div key={index} className="">
+    <div key={index} className="pb-20">
       <img src={items.imgSrc} alt="" style={{ width: '100%', height: '80%' }} className="rounded-lg hover:scale-105 transition duration-300 cursor-pointer "/>
       <h1 className="font-mono pt-3 text-xl text-blue-500">{items.head}</h1>
       <p className="font-mono ">{items.headingText}</p>
--- src/header.tsx
+++ src/header.tsx
@@ -13,26 +13,26 @@ const Header: React.FC = () => {
     <header className='fixed h-20 z-10 top-0 items-center w-full flex 
     gap-4 justify-evenly bg-white'>
       <div id="logo">
-        <a href="/"><h1 className='text-2xl italic'>Peace<span className='text-blue-500 text-3xl font-bold'>Resort</span></h1></a>
+        <a href="/learning-tailwind/"><h1 className='text-2xl italic'>Peace<span className='text-blue-500 text-3xl font-bold'>Resort</span></h1></a>
       </div>
       <img src={Bars} alt="" onClick={handleNav} className={`  bg-white md:hidden block cursor-pointer w-7`}/>
 
 <nav className={`transform transition-transform duration-500 ease-in-out ${isVisible ? 'translate-x-9' : 'translate-x-[700%]'} fixed right-0 top-0 h-full w-64 bg-white md:block md:hidden block cursor-pointer `}>
  <ul className='flex flex-col text-lg gap-4 font-bold font-mono p-4'>
- <img src={Arrow} alt="arrow-left" onClick={handleNav} className='w-10'/>
-          <li className='hover:text-blue-500'><a href="/">Home</a></li>
-          <li className='hover:text-blue-500'><a href="#">About</a></li>
-          <li className='hover:text-blue-500'><a href="#">Contact</a></li> 
-          <li className='hover:text-blue-500'><a href="#">Bookings</a></li>
-          <li className='hover:text-blue-500'><a href="#">Photos</a></li>
-          <li className='hover:text-blue-500'><a href="#">Direction</a></li>
+ <img src={Arrow} alt="arrow-left" onClick={handleNav} className='w-10 hover:bg-blue-200'/>
+          <li className='hover:text-blue-500 text-3xl'><a href="/">Home</a></li>
+          <li className='hover:text-blue-500 text-3xl'><a href="#">About</a></li>
+          <li className='hover:text-blue-500 text-3xl'><a href="#">Contact</a></li> 
+          <li className='hover:text-blue-500 text-3xl'><a href="#">Bookings</a></li>
+          <li className='hover:text-blue-500 text-3xl'><a href="#">Photos</a></li>
+          <li className='hover:text-blue-500 text-3xl'><a href="#">Direction</a></li>
         </ul>
       </nav>
       <nav className='hidden md:block'>
      
         <ul className='flex text-lg gap-4 font-bold font-mono'>
       
-          <li className='hover:text-blue-500'><a href="#">Home</a></li>
+          <li className='hover:text-blue-500 '><a href="#">Home</a></li>
           <li className='hover:text-blue-500'><a href="#">About</a></li>
           <li className='hover:text-blue-500'><a href="#">Contact</a></li>
           <li className='hover:text-blue-500'><a href="#">Bookings</a></li>
